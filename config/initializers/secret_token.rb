4bf8a6c46799e94e8d7aad8e7052e4c7c2dd5efb
KingMob <gsotero@gmail.com>
2013-06-18 02:01:48 -0300

    Comeco

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JardimIn::Application.config.secret_token = '43c7c99cdfd7cf15af778a9703c05f78fa7b23d71f23dc088a7a61eb460418dcbda268b492dd09ec3d4a698b84903506aa364818c2f2b8f0fb918b7845a26ee1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+JardimIn::Application.config.session_store :cookie_store, key: '_jardim.in_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# JardimIn::Application.config.session_store :active_record_store
